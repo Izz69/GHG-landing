dbf50ebf205e54f102ee0834d943b1245973c5ae
Rizwan Ansar <131580981+RizwanAnsar2004@users.noreply.github.com>
2024-10-06 12:58:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
             <div className="body">
 
               
-             <Navbar />
+             
               <Home />
                {/* <Secondpage /> */}
                <Thirdpage />
